69347b48715bb1b6a1dc41fb8fbb0ebf2f96a901
Richesh Gupta <richesh.gupta@kroger.com>
2022-10-08 11:54:06 +0530

    Asset mgmt models

--- EazyUpdates/User/models.py
+++ EazyUpdates/User/models.py
@@ -1,10 +1,20 @@
 from logging import PlaceHolder
 from django.db import models
 from django.contrib.auth.models import User
-# from mptt.models import MPTTModel, TreeForeignKey, TreeManager
 from treebeard.mp_tree import MP_Node
-# Create your models here.
 
+
+class Team(models.Model):
+    Name = models.CharField(max_length=30)
+    Lead = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Name
+    
+class Mentor(MP_Node):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username
+    
 #Users
 # Email - EmailFIeld
 # phone number - PhoneField
@@ -16,49 +26,21 @@ from treebeard.mp_tree import MP_Node
 # Level - 1. Executive / 2. IT Admin /3. Employee
 # Emp ID
 
+access_level = [(1,'Executive'),(2,'IT'),(3,'Employee')]
 
-class Team(models.Model):
-    Name = models.CharField(max_length=30)
-    Lead = models.ForeignKey(User,on_delete=models.CASCADE)
-    
-class Mentor(MP_Node):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    # Mentor = TreeForeignKey('self',on_delete=models.CASCADE,related_name = 'mentors')
-    # parent = models.ForeignKey('self',on_delete=models.CASCADE)
-    # TODO: Add __str__ method
-    
 class extended_user(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="users")
     Email = models.EmailField()
     Mentor = models.ForeignKey(Mentor,on_delete=models.CASCADE,related_name="mentors")
-    # Mentor = TreeForeignKey('self',on_delete=models.CASCADE,related_name = 'children')
     Mentee = models.ForeignKey(User,on_delete=models.CASCADE,related_name="mentees")
     Designation=models.CharField(max_length=20)
     YOJ = models.DateField()
     Team = models.ForeignKey(Team,on_delete=models.CASCADE)
     EMPID = models.CharField(max_length=10)
+    Level = models.PositiveSmallIntegerField(choices=access_level,default=3)
+    
+    def __str__(self):
+        return self.user.username
 
         
-
-
-
-# Asset Model
-# Serial ID
-# Asset Description
-# owner - Foreign Key - User
-
-# Asset Mgmt (ticket)
-# Ticket ID
-# Serial ID
-# Owner - FK - User
-# Description
-# Status - Enum {
-# Approval from {{Akshat}}
-# Asset collection
-# Asset Review By {{Anupam}}
-# Replaced / Repair / Collected
-# Employee Approval
-# }
-
-
 
--- EazyUpdates/asset_mgmt/models.py
+++ EazyUpdates/asset_mgmt/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Asset(models.Model):
+    SerialID = models.CharField(max_length=20)
+    Description = models.TextField()
+    Owner = models.ForeignKey(to=User,on_delete=models.CASCADE)
+
+# Asset Model
+# Serial ID
+# Asset Description
+# owner - Foreign Key - User
+
+status = [(1,'Approved'),(2,'Asset collection'),(3,'Asset Review'),(4,'Replaced / Repaired'),(5,'Verified')]
+
+class AssetTicket(models.Model):
+    Asset = models.OneToOneField(Asset,on_delete=models.CASCADE)
+    status = models.SmallIntegerField(choices=status)
+    
+
+
+
