0e6456d0a47224802ebd6e323bdf9bcb555dc3fe
sarora <sukant_arora@mcafee.com>
2022-10-08 19:29:15 +0530

    added email

--- EazyUpdates/asset_mgmt/views.py
+++ EazyUpdates/asset_mgmt/views.py
@@ -17,12 +17,13 @@ def assetRaiseticket(request,assetID):
     try:
         asset_action = request.GET["rating"]
         AssetObj = Asset.objects.filter(id=assetID)[0]
+        owner_name = AssetObj.Owner.username
         if asset_action=="return":
             return_reason = request.GET["feedback_ok"]
             AssetTicket.objects.create(status=0,Asset=AssetObj,reason = return_reason,owner = request.user)
             send_mail(
                 'Asset Return approval',
-                '{Name}, {EmployeeId}, has raise a request for the asset return.\n Please approve the request.',
+                owner_name + 'has raise a request for the asset return.\n Please approve the request.',
                 'sukant.2772001@gmail.com',
                 ['sharif.nawaz@zopsmart.com'],
                 fail_silently=False,
@@ -32,6 +33,14 @@ def assetRaiseticket(request,assetID):
             damage_type = request.GET["testimonial"]
             desc_damage = request.GET["feedback_great"]
             AssetTicket.objects.create(status=0,Asset=AssetObj,reason = desc_damage,damagetype=damage_type,owner = request.user)
+            send_mail(
+                'Asset Replacement approval',
+                owner_name + ' has raise a request for the asset replacement.\n\nDamage_Desc: '+ desc_damage +'\ndamage_type: '+damage_type+'\nPlease approve the request.',
+                'sukant.2772001@gmail.com',
+                ['sharif.nawaz@zopsmart.com'],
+                fail_silently=False,
+            )
+            return Assethome(request)
     except:
         pass
     return render(request,"asset_mgmt/ticket.html",{})
